a39fafa9fc54a5b3c9df164eb8b07c5823c59291
deeppise <125956137+deeppise@users.noreply.github.com>
2023-02-27 09:09:15 +0530

    Add files via upload

---
+++ Drag_Drop1.java
@@ -0,0 +1,25 @@
+package com.Practice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Drag_Drop1 {
+
+	public static void main(String[] args)
+	{
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.google.com/doodles");
+		WebElement ele = driver.findElement(By.linkText("Valentine's Day 2023"));
+		Actions a=new Actions(driver);
+		a.click(ele).perform();
+
+	}
+
+}
